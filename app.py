4b6279f70a2cc4e836dae2333dc65688d646ef73
fs-dualis <fsuhle@dualis-it.de>
2024-02-13 17:29:22 +0100

    update logo die dritte

--- app.py
+++ app.py
@@ -26,11 +26,11 @@ bp = Blueprint("routes", __name__, static_folder="static", template_folder="stat
 
 # UI configuration (optional)
 UI_TITLE = os.environ.get("UI_TITLE") or "Contoso"
-UI_LOGO = os.environ.get("UI_LOGO")
-UI_CHAT_LOGO = os.environ.get("UI_CHAT_LOGO")
+UI_LOGO = "/favicon.ico" #os.environ.get("UI_LOGO")
+UI_CHAT_LOGO = "/favicon.ico" #os.environ.get("UI_CHAT_LOGO")
 UI_CHAT_TITLE = os.environ.get("UI_CHAT_TITLE") or "Start chatting"
 UI_CHAT_DESCRIPTION = os.environ.get("UI_CHAT_DESCRIPTION") or "This chatbot is configured to answer your questions"
-UI_FAVICON = os.environ.get("UI_FAVICON") or "/favicon.ico"
+UI_FAVICON = "/favicon.ico" #os.environ.get("UI_FAVICON") or "/favicon.ico"
 UI_SHOW_SHARE_BUTTON = os.environ.get("UI_SHOW_SHARE_BUTTON", "true").lower() == "true"
 
 def create_app():
